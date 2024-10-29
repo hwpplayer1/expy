0a7b98d1aee4331a776246dea46bf768c1740e02
Mert Gör <mertgor@masscollabs.xyz>
2024-10-29 18:45:04 +0300

    ExPy:Experimental Python code

---
+++ habibi.py
@@ -0,0 +1,8 @@
+while True:
+    your_type = input("Type something (type 'q' to quit): ")
+    if your_type == 'q':
+        break
+    else:
+        while True:
+            print(" ya habibi ! ")
+        
